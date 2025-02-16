cadc3f8307038600e032b9791e8d180312224443
Luis27cc <luiscampodel@gmail.com>
2025-02-16 23:46:24 +0100

    Delete src directory

--- src/src/pages/Home.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Home() {
-  return <h1>Bienvenido a #SoyReal</h1>;
-}
-
-export default Home;
--- src/src/pages/src/pages/TestAutoestima.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function TestAutoestima() {
-  return <h1>Test de Autoestima</h1>;
-}
-
-export default TestAutoestima;
