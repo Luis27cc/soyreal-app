5fbe1d092891cfc3db772f97ecd95f1ed34e4815
Luis27cc <luiscampodel@gmail.com>
2025-02-16 23:45:56 +0100

    Delete src/pages directory

--- src/pages/Galeria.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Galeria() {
-  return <h1>Galería de Fotos #SoyReal</h1>;
-}
-
-export default Galeria;
--- src/pages/src/pages/src/pages/Recursos.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Recursos() {
-  return <h1>Recursos sobre Autoestima</h1>;
-}
-
-export default Recursos;
